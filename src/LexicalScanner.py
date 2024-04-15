b9561e93a121caf852e6814d06382f2fce752cd2
NQuang <langphong01@gmail.com>
2024-04-16 00:18:23 +0700

    update

--- src/LexicalScanner.py
+++ src/LexicalScanner.py
@@ -7,8 +7,9 @@ class Token:
 
 class LexicalScanner:    
     KEYWORD = {"boolean", "break", "continue", "else", "for", "float", "if", "int", "return", "void", "while"}
-    OPERATOR = "&|+-*/%=<>!:"
-    SEPARATOR = "[]{}(),;\""
+    OPERATOR = {"+","-","*","/","=","==","!=","<", "<=",">",">=","||","&&","!"}    
+    OPERATOR_CHARSET = "&|+-*/%=<>!:"
+    SEPARATOR = "[]{}(),;"
     ALPHABET = "abcdefghijklmnopqrstuvwxyz_QWERTYUIOPASDFGHJKLZXCVBNM"
     NUMBER = "0123456789"
 
@@ -46,6 +47,7 @@ class LexicalScanner:
             return False
         return True
     
+    
 class LexicalType(Enum):
     Identifier = 0
     Keyword = 1
@@ -65,8 +67,12 @@ def scan(file_path):
     words = []
 
     current_token = ""
+    current_operator = ""
     in_single_line_comment = False
     in_multi_line_comment = False
+    in_string = False
+    str_content = ""
+    start_operator = False
     for char_index, char in enumerate(content):
         if in_single_line_comment:
             if char == '\n':
@@ -82,13 +88,25 @@ def scan(file_path):
         elif char == '/' and content[char_index + 1] == '*':
             in_multi_line_comment = True
             continue
+        elif char == '"' and not (in_single_line_comment or in_multi_line_comment):
+            str_content += char
+            if in_string:
+                if str_content:                
+                    tokens.append(Token(LexicalType.StrLiteral, str_content))
+                    words.append(str_content)
+                str_content = ""
+            in_string = not in_string            
+            continue
+                
+        if in_string: 
+            str_content += char
+            continue
         
-        if char.isspace() or char in LexicalScanner.SEPARATOR or char in LexicalScanner.OPERATOR:
+        if char.isspace() or char in LexicalScanner.SEPARATOR or char in LexicalScanner.OPERATOR_CHARSET:
             if current_token:
                 if LexicalScanner.isKeyWord(current_token):
                     tokens.append(Token(LexicalType.Keyword, current_token))
-                    words.append(current_token)
-                    
+                    words.append(current_token)                    
                 elif LexicalScanner.isNumber(current_token):
                     if '.' in current_token:
                         if LexicalScanner.isRealNumber(current_token): 
@@ -102,6 +120,9 @@ def scan(file_path):
                 elif LexicalScanner.isAlphatbet(current_token):
                     tokens.append(Token(LexicalType.Identifier, current_token))
                     words.append(current_token)
+                elif LexicalScanner.isOperator(current_token):
+                    tokens.append(Token(LexicalType.Operator, current_token))
+                    words.append(current_token)
                 else:
                     print("Unknown Token: ", current_token)
                 current_token = ""
@@ -109,9 +130,22 @@ def scan(file_path):
             if char in LexicalScanner.SEPARATOR:
                 tokens.append(Token(LexicalType.Separator, char))
                 words.append(char)
-            elif char in LexicalScanner.OPERATOR:
-                tokens.append(Token(LexicalType.Operator, char))
-                words.append(char)
+            if char in LexicalScanner.OPERATOR_CHARSET:             
+                current_operator += char                                                   
+                if start_operator:
+                    start_operator = False
+                    tokens.append(Token(LexicalType.Operator, current_operator))
+                    words.append(current_operator)  
+                    current_operator = ""
+                    continue
+                if content[char_index + 1] in LexicalScanner.OPERATOR_CHARSET:
+                    start_operator = True    
+                    continue
+                tokens.append(Token(LexicalType.Operator, current_operator))
+                words.append(current_operator)  
+                current_operator = ""
+                
+                    
         else:
             current_token += char
 
--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ from LexicalScanner import scan
 def main():
     file_path = input("Nhập đường dẫn tệp chương trình Visual C: ")
 
-    tokens = scan(file_path)
+    words = scan(file_path)
     ofile_path = file_path[:-3] + ".vctok"
     with open(ofile_path, 'w') as output_file:
-        for token in tokens:
-            output_file.write(f"{token}\n")
+        for word in words:
+            output_file.write(f"{word}\n")
+        output_file.write("$")
     print(f"Kết quả đã được ghi vào tệp {ofile_path}")
 
 if __name__ == "__main__":
